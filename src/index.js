8e3c5f5b7010bbe4e44bd1d3cdd747c4c55f92e7
DmitryIP <dmitryip@tut.by>
2020-03-01 16:16:37 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = String(Math.abs(n));
+    let r = '';
+    for (let i = (n.length - 1); i >= 0; i--) {
+        r += n[i];
+
+    }
+    return +r;
 }
