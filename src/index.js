a08199ea4906cd11814777369fa6dd58813668ed
DmitryIP <dmitryip@tut.by>
2020-10-20 12:31:26 +0300

    add new function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 module.exports = function reverse(n) {
-    n = String(Math.abs(n));
-    let r = '';
-    for (let i = (n.length - 1); i >= 0; i--) {
-        r += n[i];
-
-    }
-    return +r;
+    let str = String(Math.abs(n));
+    return +str.split('').reverse().join('');
 }
